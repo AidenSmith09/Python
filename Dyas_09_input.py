0543d4534bd8bbba76888e4a186f66d1c8255312
AidenSmith09 <aidenemail09@gmail.com>
2018-10-08 15:05:46 +0800

    Update commit

--- Dyas_09_input.py
+++ Dyas_09_input.py
@@ -1,6 +1,6 @@
 # 用户输入
 # 使用input()函数进行变量存储
-
+"""
 message = input("Tell me something, and i will repeat it back to you:")
 print(message)
 message = input("输入中文可以吗：")
@@ -17,7 +17,6 @@ prompt += "\nWhat is your first name?"
 name = input(prompt)
 print("\n" + name + "!")
 
-
 # 使用 int() 方法获取数值输入
 # input()直接输入数字，都是字符串类型。所以需要使用int()进行类型转换
 height = input("How tall are you, in inches?")
@@ -29,3 +28,31 @@ else:
 
 # 如果使用Python2.7，获取数瑞应该使用`raw_input()`
 # Python2.7 中也包含函数input(),但是，它将输入的内容解读为代码，并尝试运行他们。
+"""
+# 求余数
+number = input("Enter a number, and I'll tel you if it's even or odd:")
+number = int(number)
+if number % 2 == 0:
+    print("\nThe number " + str(number) + " is even.")
+else:
+    print("\n The number " + str(number) + " is odd.")
+
+
+# 测试
+question = input("请问需要什么样的汽车？")
+print("Let me see if I can find you a SuBaru.")
+
+
+Food = input("请问多少人需要用餐？")
+Food = int(Food)
+if Food > 8:
+    print("抱歉，没有8人以上的位置。")
+else:
+    print("ok, 请跟我来")
+
+number = input("请输入一个数字")
+number = int(number)
+if number % 10 == 0:
+    print(str(number) + "是10的倍数")
+else:
+    print(str(number) + "不是10的倍数")
