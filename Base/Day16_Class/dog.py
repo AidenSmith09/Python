84ce8ed3306409cb49897fddea43d58265654cb1
AidenSmith09 <aidenemail09@gmail.com>
2019-10-09 22:37:48 +0800

    update content

--- Base/Day16_Class/dog.py
+++ Base/Day16_Class/dog.py
@@ -7,8 +7,9 @@
 
 '''
 
+
 class Dog():  # 根据类来创建对象，被称之为实例化。可以理解为文档的模板。
-    def __init__(self, name, age): #这是一个特殊的方法，第一个参数永远是self。有了它，在创建‘实例对象’的时候，就不能传入空的参数了。
+    def __init__(self, name, age):  # 这是一个特殊的方法，第一个参数永远是self。有了它，在创建‘实例对象’的时候，就不能传入空的参数了。
         """
         初始化属性name 和 age；
         初始化的意思是：创建新变量后为变量赋值后的流程，叫初始化。变量赋为默认值，把控件设为默认状态，把没准备的准备好
@@ -26,13 +27,13 @@ class Dog():  # 根据类来创建对象，被称之为实例化。可以理解�
         print(self.name.title() + " rolled over!")
 
 
-my_dog = Dog('willie', 6) # 创建一个实例对象
+my_dog = Dog('willie', 6)  # 创建一个实例对象
 print("My dog's name is "+my_dog.name.title()+".")
 print("My dog is "+str(my_dog.age)+" years old.")
-my_dog.sit()        #对象访问属性
-my_dog.roll_over()  #对象访问属性
+my_dog.sit()  # 对象访问属性
+my_dog.roll_over()  # 对象访问属性
 
-your_dog = Dog('lucy',3)
+your_dog = Dog('lucy', 3)
 print("\nYour dog's name is "+your_dog.name.title()+".")
 print("your dog is "+str(your_dog.age)+" years old.")
 your_dog.sit()
